980dbacd88593c607f906e892b147c3c23248e52
blaze56768 <79693666+blaze56768@users.noreply.github.com>
2024-09-11 13:49:50 +0530

    Update train_classifier.py

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -1,7 +1,7 @@
 import hydra
 from models import transformer_autoencoder
 from omegaconf import DictConfig
-from datamodules import TabularDataModule
+from datamodules.tabular_datamodule import TabularDataModule
 from models import lr_classifier
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
@@ -10,29 +10,42 @@ from utils import utils_funcs
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks import LearningRateMonitor,ModelCheckpoint
 
-
-@hydra.main(config_path = "../configs/pretrain",config_name ="config.yaml")  
+@hydra.main(version_base=None, config_path="../configs/pretrain", config_name="config.yaml")
 def train(cfg : DictConfig)->None:
-    wandb_logger = WandbLogger(project=cfg.downstream_wandb,group="toy",job_type = "train")
+    wandb_logger = WandbLogger(project=cfg.train_params.downstream_wandb, group="toy", job_type = "train")
     
     print("✅Initializing the data loader...")
-    sslloader = TabularDataModule(cfg=cfg)   
+    data_module = TabularDataModule(cfg=cfg, name_labels = cfg.datamodule_params.name_labels, features_numerical=cfg.datamodule_params.features_numerical, features_categorical=cfg.datamodule_params.features_categorical)
     
 
-    sslloader.setup("fit")
-    sslloader.setup("val")
+    data_module.setup("fit")
+    data_module.setup("validate")
     
-    cfg.lr_step_size = int(sslloader.dataset_train.data.shape[0]/cfg.datamodule_params.batch_size)*cfg.datamodule_params.downstream_n_epochs
+    cfg.train_params.lr_step_size = int(data_module.train_dataset.data.shape[0]/cfg.datamodule_params.batch_size)*cfg.train_params.downstream_n_epochs
     
-    train_loader =sslloader.train_dataloader()
-    val_dataloader =sslloader.val_dataloader()
+    train_loader = data_module.train_dataloader()
+    val_dataloader = data_module.val_dataloader()
        
     print("✅Setup phase")  
-    
     model = lr_classifier.LogisticRegressionClassifier(cfg=cfg)
+
     lr_monitor = LearningRateMonitor(logging_interval='step')
-    checkpoint_callback = ModelCheckpoint(dirpath=cfg.downstream_checkpoint, save_top_k=1, monitor="train/ap", mode="max", filename=cfg.train_params.filename)
-    trainer = L.Trainer(max_epochs=cfg.train_params.downstream_n_epochs, logger = wandb_logger, check_val_every_n_epoch=cfg.train_params.val_epochs, callbacks=[checkpoint_callback,lr_monitor])
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=cfg.downstream_checkpoint, 
+        save_top_k=1, 
+        monitor="train/ap", 
+        mode="max", 
+        filename=cfg.train_params.filename)
+    
+
+    trainer = L.Trainer(
+        max_epochs=cfg.train_params.downstream_n_epochs, 
+        logger = wandb_logger, 
+        check_val_every_n_epoch=cfg.train_params.val_epochs, 
+        callbacks=[checkpoint_callback,lr_monitor]
+        )
+    
     wandb_logger.watch(model,log="all")
     trainer.fit(model=model,train_dataloaders=train_loader,val_dataloaders=val_dataloader)
     checkpoint_callback.best_model_path
